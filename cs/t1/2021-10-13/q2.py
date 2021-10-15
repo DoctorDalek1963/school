e6ccd4b02090b08e10d0d4fed15cf5b44a252aaa
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2021-10-15 10:44:10 +0100

    Simplify find_deletion_number()

--- cs/t1/2021-10-13/q2.py
+++ cs/t1/2021-10-13/q2.py
@@ -8,10 +8,7 @@ import re
 
 def find_deletion_number(s: str) -> int:
     """Find how many A and B characters must be deleted to remove all sequences of adjacent characters."""
-    num_a = len(''.join(re.findall('(?<=A)A+', s)))
-    num_b = len(''.join(re.findall('(?<=B)B+', s)))
-
-    return num_a + num_b
+    return len(''.join(re.findall('(?<=A)A+', s) + re.findall('(?<=B)B+', s)))
 
 
 if __name__ == "__main__":
