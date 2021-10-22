75bf60e548ee3b28cd9c8e90d7c20e65d175ee86
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2021-10-22 14:13:53 +0100

    Add tests for exceptions and distance matrix

--- fm/d1/graphs/test_library.py
+++ fm/d1/graphs/test_library.py
@@ -55,6 +55,56 @@ class TestLibrary(unittest.TestCase):
         for v in [a, b, c, d, e]:
             self.assertIn(v, g.vertices)
 
+    def test_vertex_already_added_error(self) -> None:
+        """Test the raising of VertexAlreadyAddedError."""
+        g = Graph()
+        a, b, c, d, e = create_vertices('A B C D E')
+
+        g.add_vertices(a, b, c, d, e)
+
+        with self.assertRaises(VertexAlreadyAddedError):
+            g.add_vertex(a)
+            g.add_vertex(b)
+            g.add_vertex(c)
+            g.add_vertex(d)
+            g.add_vertex(e)
+            g.add_vertices(a, b, c, d, e)
+
+    def test_vertex_doesnt_exist_error(self) -> None:
+        """Test the raising of VertexDoesntExistError when adding an edge."""
+        g = Graph()
+        a, b, c, d, e = create_vertices('A B C D E')
+
+        with self.assertRaises(VertexDoesntExistError):
+            g.add_edge(a, b)
+            g.add_edge(a, c)
+            g.add_edge(a, d)
+            g.add_edge(a, e)
+            g.add_edge(b, c)
+            g.add_edge(b, d)
+            g.add_edge(b, e)
+            g.add_edge(c, d)
+            g.add_edge(c, e)
+            g.add_edge(d, e)
+
+    def test_graph_matrix(self) -> None:
+        """Test the matrix given by str(Graph)."""
+        g = Graph()
+        a, b, c, d, e = create_vertices('A B C D E')
+
+        g.add_vertices(a, b, c, d, e)
+
+        expected_matrix = '-\t3\t-\t-\t4\n2\t-\t9\t-\t12\n-\t9\t-\t4\t-\n7\t-\t-\t-\t-\n-\t12\t-\t-\t-'
+        g.add_edge(a, b, 3, True)
+        g.add_edge(a, e, 4, True)
+        g.add_edge(b, a, 2, True)
+        g.add_edge(b, c, 9, False)
+        g.add_edge(b, e, 12, False)
+        g.add_edge(c, d, 4, True)
+        g.add_edge(d, a, 7, True)
+
+        self.assertEqual(str(g), expected_matrix)
+
 
 if __name__ == "__main__":
     unittest.main()
