e7c7057be510c031043972a2ade80a27fd23a851
DoctorDalek1963 <dyson.dyson@icloud.com>
2023-03-31 14:59:36 +0100

    Remove option to allow negativity

--- fm/d1/simplex/src/lin_prog/config.rs
+++ fm/d1/simplex/src/lin_prog/config.rs
@@ -7,9 +7,6 @@ use tracing::instrument;
 /// A simple config struct to handle options for the [`LinProgSystem`].
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct Config {
-    /// Whether to include the non-negativity constraints (x, y, z ≥ 0).
-    pub include_non_negativity: bool,
-
     /// Does this system require integer solutions?
     pub integer_solutions: bool,
 }
@@ -17,7 +14,6 @@ pub struct Config {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            include_non_negativity: true,
             integer_solutions: false,
         }
     }
@@ -27,18 +23,16 @@ impl Config {
     /// Build the config from user input using `inquire`.
     #[instrument]
     pub fn build_from_user() -> Result<Self> {
-        const NON_NEGATIVITY: &str = "Include non-negativity";
         const INTEGER_SOLUTIONS: &str = "Require integer solutions";
 
         let selected = MultiSelect::new(
             "Please enable or disable configurations:",
-            vec![NON_NEGATIVITY, INTEGER_SOLUTIONS],
+            vec![INTEGER_SOLUTIONS],
         )
-        .with_default(&[0])
+        .with_default(&[])
         .prompt()?;
 
         Ok(Self {
-            include_non_negativity: selected.contains(&NON_NEGATIVITY),
             integer_solutions: selected.contains(&INTEGER_SOLUTIONS),
         })
     }
